af8ab93575cda34d5ac4eaa93ea418b96747442a
Alek Sai <lenyapugachev@me.com>
2021-07-03 01:28:36 +0700

    Initial Commit

---
+++ TV/TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVApp.swift
+//  TV
+//
+//  Created by Alek Sai on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
